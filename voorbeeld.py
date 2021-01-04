0dcf490977976ad98f9e195b6035b30eba3bfd02
AlanaAckermans <64835172+AlanaAckermans@users.noreply.github.com>
2021-01-04 10:57:02 +0100

    Add files via upload

---
+++ voorbeeld.py
@@ -0,0 +1,26 @@
+#!usr/bin/env python
+
+#INFO
+__author__ = "Alana Ackermans"
+__version__ = "0.0.1"
+
+#LIBRARIES
+
+#testcode voor simulate
+
+sensordata = [2,7,5,6,8,9,5,2,3,4,1,]
+
+def gemiddelde():
+    middel = sum(sensordata)/len(sensordata)
+    print(middel)
+
+#FUNCTIONS
+
+
+#MAIN FUNCTION
+def main():
+    gemiddelde()
+
+#START PROGRAM
+if __name__ == '__main__':
+    main()
